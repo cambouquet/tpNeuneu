80027c2dfc32fa7cd80df5ded9be06fbb50abcc5
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-11-04 10:44:14 +0100

    amelioration de la classe positionnable

--- src/com/ei3info/tp/lofteurs/ObjetPositionnable.java
+++ src/com/ei3info/tp/lofteurs/ObjetPositionnable.java
@@ -11,13 +11,40 @@ import java.awt.Graphics;
  */
 public abstract class ObjetPositionnable implements ObjetDessinable {
 
-	private int posX;
-	private int posY;
+	/**
+	 * Abscisse en nombre de cases, à partir de 0.
+	 */
+	protected int posX;
+
+	/**
+	 * Ordonnees en nombre de cases, à partir de 0.
+	 */
+	protected int posY;
+	
+	/**
+	 * Largeur d'une case en pixel.
+	 */
+	protected int tailleX;
+
+	/**
+	 * Hauteur d'une case en pixel.
+	 */
+	protected int tailleY;
 	
 	@Override
 	public void dessinerObjet(Graphics g) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public int getX()
+	{
+		return this.posX;
+	}
+	
+	public int getY()
+	{
+		return this.posY;
 	}
 
 }
