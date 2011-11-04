c22d033bc11aae28726466c6326b8dbbeaa5e0bc
Antoine Sellam <sphoenixx@gmail.com>
2011-11-04 11:27:33 +0100

    Creation de la methode seDeplacer() pour les voraces

--- src/com/ei3info/tp/lofteurs/Vorace.java
+++ src/com/ei3info/tp/lofteurs/Vorace.java
@@ -6,4 +6,15 @@ public class Vorace extends Neuneu {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void seDeplacer() {
+		// Recherche de la nourriture la plus proche
+		int[] nextF = new int[2];
+		nextF = trouverNourriturePlusProche(this);
+		// Pathfinding basique
+		int mvHandleX = nextF[0]/Math.abs(nextF[0]);
+		int mvHandleY = nextF[1]/Math.abs(nextF[1]);
+		// Déplacement
+		this.posX = this.posX + mvHandleX;
+		this.posY = this.posY + mvHandleY;
+	}
 }
