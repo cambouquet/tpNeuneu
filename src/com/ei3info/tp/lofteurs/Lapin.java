3d32d5d46ef3597eabbd136612439b6b3634b5a5
Antoine Sellam <sphoenixx@gmail.com>
2011-11-04 11:50:06 +0100

    Creation de la methode seDeplacer() pour les lapins

--- src/com/ei3info/tp/lofteurs/Lapin.java
+++ src/com/ei3info/tp/lofteurs/Lapin.java
@@ -1,5 +1,41 @@
-package com.ei3info.tp.lofteurs;
+package src.com.ei3info.tp.lofteurscom.ei3info.tp.lofteurs;
 
 public class Lapin extends Neuneu{
+	public Lapin(Loft loft, int x, int y) {
+		// TODO Auto-generated constructor stub
+	}
 
+	public void seDeplacer() {
+		// Le principe du lapin est qu'il cherche à se reproduire avant de chercher à manger. 
+		// S'il n'a pas suffisamment d'énergie pour se déplacer et se reproduire, il cherchera d'abord à manger. 
+		
+		// Recherche du neuneu le plus proche
+		int[] nextN = new int[2];
+		nextN = trouverNeuneuPlusProche(this);
+		int energN = nextN[0] + nextN [1] + 2 +2;  // On suppute à ce stade que la reproduction nécessite 2 unités d'énergie, et on met 2 unités d'énergie en réserve pour ne pas se faire exclure.
+		if (this.energie < energN) {
+			// COMPORTEMENT VORACE
+			// Recherche de la nourriture la plus proche
+			int[] nextF = new int[2];
+			nextF = trouverNourriturePlusProche(this);
+			// Pathfinding basique
+			int mvHandleX = nextF[0]/Math.abs(nextF[0]);
+			int mvHandleY = nextF[1]/Math.abs(nextF[1]);
+			// Déplacement
+			this.posX = this.posX + mvHandleX;
+			this.posY = this.posY + mvHandleY;
+		}
+		else {
+			// COMPORTEMENT CANNIBALE (twist!)
+			int[] nextN = new int[2];
+			nextN = trouverNeuneuPlusProche(this);
+			// Pathfinding basique
+			int mvHandleX = nextN[0]/Math.abs(nextN[0]);
+			int mvHandleY = nextN[1]/Math.abs(nextN[1]);
+			// Déplacement
+			this.posX = this.posX + mvHandleX;
+			this.posY = this.posY + mvHandleY;
+		}
+		
+	}
 }
