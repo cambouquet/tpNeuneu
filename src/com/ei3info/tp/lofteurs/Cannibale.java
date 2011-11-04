b82c819e10e7bb37a4dbc3272b86fb22980c3719
Antoine Sellam <sphoenixx@gmail.com>
2011-11-04 11:36:35 +0100

    Creation de la methode seDeplacer() pour les cannibales

--- src/com/ei3info/tp/lofteurs/Cannibale.java
+++ src/com/ei3info/tp/lofteurs/Cannibale.java
@@ -5,5 +5,28 @@ public class Cannibale extends Neuneu {
 	public Cannibale(Loft loft, int x, int y) {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void seDeplacer() {
+		// Recherche de la nourriture la plus proche
+		int[] nextF = new int[2];
+		nextF = trouverNourriturePlusProche(this);
+		// Recherche du neuneu le plus proche
+		int[] nextN = new int[2];
+		nextN = trouverNeuneuPlusProche(this);
+		// Détermination de la source la plus proche
+		double distF = Math.sqrt(nextF[0]^2 + nextF[1]^2);
+		double distN = Math.sqrt(nextN[0]^2 + nextN[1]^2);
+		if (distF < distN) {
+			nextN[0] = 0; // Il fallait bien mettre quelque chose.
+		}
+		else {
+			nextF = nextN;
+		}
+		// Pathfinding basique
+		int mvHandleX = nextF[0]/Math.abs(nextF[0]);
+		int mvHandleY = nextF[1]/Math.abs(nextF[1]);
+		// Déplacement
+		this.posX = this.posX + mvHandleX;
+		this.posY = this.posY + mvHandleY;
+	}
 }
