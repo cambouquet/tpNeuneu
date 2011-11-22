36223c9d6f713347fa9b8d86e273ab8ed75e3752
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-11-22 22:25:12 +0100

    Ajoute la javadocs et les commentaires a la classe ResumePanel

--- src/com/ei3info/tp/lofteurs/ResumePanel.java
+++ src/com/ei3info/tp/lofteurs/ResumePanel.java
@@ -15,24 +15,43 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 /**
+ * Panneau affichant les commentaires sur la saison en cours.
  * @author Camille Bouquet
  * @author Antoine Sellam
  */
 public class ResumePanel extends JPanel
 {
     /**
-     * 
+     * Generated default serial UID.
      */
     private static final long serialVersionUID = 1L;
+    
+    /**
+     * Le nombre de commentaires actuellement affichés.
+     */
     private int         nombreCommentaires = 0;
+    
+    /**
+     * Le panneau qui contient effectivement les commentaires.
+     */
     private JPanel      resumeContenuPanel;
+    
+    /**
+     * Le scroll pane qui contient le panneau avec les commentaires.
+     */
     private JScrollPane jsp;
 
+    /**
+     * Constructeur.
+     */
     public ResumePanel()
     {
         creerResumePanel();
     }
     
+    /**
+     * Créé le panneau.
+     */
     private void creerResumePanel()
     {
         JLabel titreResumePanel = new JLabel("Résumé des événements");
@@ -49,14 +68,31 @@ public class ResumePanel extends JPanel
         this.add(jsp, BorderLayout.CENTER);
     }
 
+    /**
+     * Affiche un nouvelle événement à l'écran.
+     * 
+     * @param evenement
+     *          Le texte à afficher.
+     * @param couleur
+     *          La couleur du texte à afficher.
+     * @param sauterLigne
+     *          Indique si l'on doit sauter une ligne après le texte.
+     * @param positionX
+     *          Donne la position à afficher dans la ligne.
+     * @param finLigne
+     *          Indique si le message est le dernier messsage de la ligne.
+     */
     public void
             afficherEvenement(String evenement, Color couleur, boolean sauterLigne, int positionX, boolean finLigne)
     {
         JLabel lEvenement = new JLabel(evenement);
         lEvenement.setForeground(couleur);
 
+        // Saut de ligne
         int verticalInset = (sauterLigne) ? 15 : 2;
         Insets ligneInsets = new Insets(2, 2, verticalInset, 5);
+        
+        // Fin de la ligne
         int longueur = (finLigne) ? GridBagConstraints.REMAINDER : 1;
 
         resumeContenuPanel.add(lEvenement, new GridBagConstraints(positionX, nombreCommentaires, longueur, 1, 0.0, 0.0,
@@ -72,6 +108,9 @@ public class ResumePanel extends JPanel
         jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMaximum());
     }
 
+    /**
+     * Efface tous les commentaires.
+     */
     public void clear()
     {
         resumeContenuPanel.removeAll();
